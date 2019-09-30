94c5d509e0aee7f37d35be6997c7490f6cfb0a2f
Daniel Marlon <danielmarlon181@gmail.com>
2019-09-30 13:41:32 -0300

    comentário

---
+++ index.php
@@ -0,0 +1,39 @@
+ <html>
+  <form  action="mydata.php" method="POST">
+    <fieldset>
+
+        <legend>Informações</legend>
+        <p>
+            <label for="nome">Nome: <em class="required">(obrigatório)</em></label>
+            <input name="nome" id="nome" type="text" />
+        </p>
+
+        <p>
+          <label for="endereco">endereco:</label>
+          <input name="endereco" id="endereco" type="text" />
+        </p>
+
+
+        <p>
+          <label for="email">Email Address:</label>
+          <input name="email" id="email" type="text" />
+        </p>
+
+
+      <p>
+        <label for="idade">idade:</label>
+        <input name="idade" id="idade" type="text" />
+      </p>
+
+      <p>
+        <label for="telefone">telefone:</label>
+        <input name="telefone" id="telefone" type="text" />
+      </p>
+
+    </fieldset>
+
+    <p>
+        <input type="submit"
+    </p>
+
+</form>
